11a52d759736c713d81ab5d49ba6f8a64a407f31
UjjwalSinghY <ujjwalsingh24090@gmail.com>
2025-10-21 20:18:02 +0530

    adding ReverseArray.java file

---
+++ src/basic/ReverseArray.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+        int[] a={1,2,3,4,5};
+        for(int i=0;i<a.length/2;i++){
+            a[i]=a[i]+a[a.length-1-i];
+            a[a.length-1-i]=a[i]-a[a.length-1-i];
+            a[i]=a[i]-a[a.length-1-i];
+        }
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
